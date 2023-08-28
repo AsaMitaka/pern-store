73cffec7219cb1b8613723bd7dcf3cae27f4e359
AsaMitaka <cepelits@gmail.com>
2023-08-28 23:16:10 +0300

    server fisrt route

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// router.use('/user');
+// router.use('/type');
+// router.use('/brand');
+// router.use('/device');
+
+module.exports = router;
