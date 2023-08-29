64c8927866e0b8815e7b04fb1764d0456bfd290f
AsaMitaka <cepelits@gmail.com>
2023-08-29 18:21:49 +0300

    server: index root

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 require('dotenv').config();
 const sequelize = require('./db');
 const models = require('./models/models');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/errorHandling.middleware');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload());
+app.use('/api', router);
+app.use(errorHandler);
 
 const start = async () => {
   try {
