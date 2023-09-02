56c1b866c39648860901ff38289ebf3762d0fb2f
AsaMitaka <cepelits@gmail.com>
2023-09-02 17:33:48 +0300

    server minor fix

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const TypeController = require('../controllers/type.controller');
 const checkRole = require('../middleware/checkRole.middleware');
 
-router.post('/', checkRole('admin'), TypeController.create);
+router.post('/', checkRole('ADMIN'), TypeController.create);
 router.get('/', TypeController.getAll);
 
 module.exports = router;
