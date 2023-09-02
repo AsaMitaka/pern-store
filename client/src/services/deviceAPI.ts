d4164a198f76aeb25cf577fc0d3b3bcb5d3b3c90
AsaMitaka <cepelits@gmail.com>
2023-09-02 17:34:55 +0300

    client: minor fix

---
+++ client/src/services/deviceAPI.ts
@@ -0,0 +1,44 @@
+import { Brand, Device, Type } from '../types/device';
+import { $host, $authHost } from './axios';
+
+export const createType = async (type: Type) => {
+  const { data } = await $authHost.post('api/type', type);
+
+  return data;
+};
+
+export const fetchType = async (): Promise<Type[]> => {
+  const { data } = await $host.get('api/type');
+
+  return data;
+};
+
+export const createBrand = async (brand: Brand) => {
+  const { data } = await $authHost.post('api/brand', brand);
+
+  return data;
+};
+
+export const fetchBrand = async (): Promise<Brand[]> => {
+  const { data } = await $host.get('api/brand');
+
+  return data;
+};
+
+export const createDevice = async (device: Device) => {
+  const { data } = await $authHost.post('api/device', device);
+
+  return data;
+};
+
+export const fetchDevice = async (): Promise<Device[]> => {
+  const { data } = await $host.get('api/device');
+
+  return data;
+};
+
+export const fetchOneDevice = async (id: string): Promise<Device> => {
+  const { data } = await $authHost.get(`api/device/${id}`);
+
+  return data;
+};
